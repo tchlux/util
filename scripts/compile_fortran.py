f2108930c5b2797e846acda62cca01e014f8fb97
Thomas Lux <thomas.ch.lux@gmail.com>
2017-10-31 22:38:35 -0400

    Revamping compilation of fortran. Should be working.

--- scripts/compile_fortran.py
+++ scripts/compile_fortran.py
@@ -13,10 +13,11 @@ import os
 algorithms_path = os.path.join(os.path.dirname(os.getcwd()), "util", "algorithms")
 algorithm_dirs = [a for a in os.listdir(algorithms_path)
                   if os.path.isdir(os.path.join(algorithms_path,a))]
+print(algorithms_path)
 for alg_name in algorithm_dirs:
     path = os.path.join(algorithms_path, alg_name)
     for f in os.listdir(path):
-        if (alg_name in f) and (".f" in f[:-3]):
+        if (alg_name in f) and (".f" in f[-4:]):
             source_file = os.path.join(path, f)
             if os.path.isfile(source_file): break
     else:
