0cf895535274b220c2c0f68d2cb4d30f0632479f
Thomas Lux <thomas.ch.lux@gmail.com>
2017-12-11 11:01:04 -0500

    Added the fmodpy 'run' command to the system module.

--- util/system.py
+++ util/system.py
@@ -1,4 +1,4 @@
-import os
+import os, sys, subprocess
 
 MAX_FILE_SIZE_BYTES = 90*(2**20)
 CHUNK_EXT = "_(part_%i_of_%i).chunk"
@@ -36,3 +36,36 @@ def disassemble(file_path, max_size_bytes=MAX_FILE_SIZE_BYTES,
                     print(" writing chunk %i of %i in '%s'..."%(c,chunks,out.name))
                 out.write(f.read(max_size_bytes))
             
+
+# Execute a blocking command with a subprocess, on completion provide
+# the return code, stdout as string, and stderr as string. This should
+# work across both Python2.7 and Python3.x as well as cross-platform.
+#  INPUT:
+#   command -- A list of strings or string (space separated) describing
+#              a standard command as would be given to subprocess.Popen
+# 
+#  OUTPUT:
+#   return_code -- Straight from the subprocess returncode
+#   stdout      -- A list of strings that are the lines of stdout 
+#                  produced by the execution of <command>
+#   stderr      -- A list of strings that are the lines of stderr
+#                  produced by the execution of <command>
+def run(command, **popen_kwargs):
+    # For Python 2.x the encoding is a string by default
+    # For Python 3.6 and later the encoding can be given as an arguemnt
+    if sys.version_info >= (3,6):
+        popen_kwargs.update( dict(encoding="UTF-8") )
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, **popen_kwargs)
+    stdout, stderr = proc.communicate()
+    # Before python 3.6, the encoding had to be handled after collection
+    if ((sys.version_info >= (3,)) and (sys.version_info[0] == 3)):
+        if (type(stdout) != str): stdout = str(stdout, encoding="UTF-8")
+        if (type(stderr) != str): stderr = str(stderr, encoding="UTF-8")
+    # Remove windows specific characters and split by the new line
+    if stdout: stdout = stdout.replace("\r","").split("\n")
+    else:      stdout = ""
+    if stderr: stderr = stderr.replace("\r","").split("\n")
+    else:      stderr = ""
+    # Return the exit code, standard out, and standard error
+    return proc.returncode, stdout, stderr
