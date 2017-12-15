f890105b8d147568058600ed5c115238742a4104
Thomas Lux <thomas.ch.lux@gmail.com>
2017-12-15 15:35:27 -0500

    Updated 'optimize' to have AMPGO, AdaptiveNormal, and Random optimizers. It's officially more useful than scipy.optimize.minimize on multi-min / large-scale problems.

---
+++ util/algorithms/voronoi_mesh/plane.py
@@ -0,0 +1,143 @@
+import random
+import numpy as np
+from scipy.optimize import minimize
+
+SMALL_NUM = .00001
+BIG_NUM = 1000000
+
+L1   = lambda x: np.sum(abs(x)**1) **(1/1)
+L2   = lambda x: np.sum(abs(x)**2) **(1/2)
+L3   = lambda x: np.sum(abs(x)**3) **(1/3)
+L4   = lambda x: np.sum(abs(x)**4) **(1/4)
+L5   = lambda x: np.sum(abs(x)**5) **(1/5)
+L6   = lambda x: np.sum(abs(x)**6) **(1/6)
+LINF = lambda x: max(abs(x))
+
+LINEAR    = lambda r: max(0,1 - r)
+QUADRATIC = lambda r: max(0,1 - r)**2
+WEIRD     = lambda r: max(0,1 - r)**1.5
+
+BASIS_SIZE = 2.0
+
+class Linn:
+    # Initialize the Linn regressor.
+    def __init__(self, radius=LINEAR, length=L2, basis_size=BASIS_SIZE):
+        self.radius = radius
+        self.length = length
+        self.basis_size = basis_size
+
+    # Fit a set of points
+    def fit(self, points, values):
+        self.points = points.copy()
+        self.values = values.copy()
+
+    # Wrapper for 'predict' that returns a single value for a single
+    # prediction, or an array of values for an array of predictions
+    def __call__(self, x:np.ndarray, *args, **kwargs):
+        single_response = len(x.shape) == 1
+        if single_response:
+            x = np.array([x])
+        if len(x.shape) != 2:
+            raise(Exception("ERROR: Bad input shape."))
+        response = np.asarray(self.predict(x, *args, **kwargs), dtype=float)
+        # Return the response values
+        return response[0] if single_response else response
+
+    # Generate a prediction for a new point
+    def predict(self, xs):
+        predictions = []
+        for x in xs:
+            weights = []
+            to_skip = set()
+            nearest_indices = np.argsort([np.sum((x-p)**2) for p in self.points])
+            # Cycle through all training points
+            for nearest_ind in range(len(self.points)): #nearest_indices:
+                # Skip points that we know will not support x
+                if nearest_ind in to_skip: 
+                    weights.append(0)
+                    continue
+                boundary = (float('inf'),)
+                # Get the nearest point
+                nearest = self.points[nearest_ind]
+                # Calculate the vectors defining separating planes for
+                # all other training points, use the closest one in
+                # the direction of 'vector' as the boundary.
+                for other_ind in range(len(self.points)):
+                    if (nearest_ind == other_ind): continue
+                    other = self.points[other_ind]
+                    divide = (other - nearest)
+                    nearest_to_divide = np.dot(nearest,divide)
+                    other_to_divide = np.dot(other,divide)
+                    x_to_divide = np.dot(x,divide)
+                    # Check to make sure that x is on the same side of
+                    # nearest as other is.
+                    sign = (other_to_divide - nearest_to_divide) * (x_to_divide - nearest_to_divide)
+                    if (sign > 0):
+                        # Identify the midpoint between (nearest + other)
+                        middle = (nearest_to_divide + other_to_divide) / 2
+                        # Identify how much farther x is than the midpoint
+                        ratio = (x_to_divide-nearest_to_divide) / (middle-nearest_to_divide)
+                        # Calculate the distance to the boundary
+                        # (using the known distance to the point)
+                        vector = (x - nearest)
+                        dist_to_x = self.length(vector)
+                        dist_to_boundary = self.length(vector/ratio)
+                        if (dist_to_boundary < boundary[0]):
+                            # ratio = dist_to_x / dist_to_boundary
+                            boundary = (dist_to_boundary, dist_to_x)
+                    else:
+                        # We know that 'other' is blocked from
+                        # reaching x by 'nearest' and should not check
+                        # for weighting because it must be 0.
+                        to_skip.add(other_ind)
+                # If there was no boundary between x and nearest
+                if (boundary[0] == float('inf')):
+                    weights.append( 1 )
+                else:
+                    new = False
+                    if new:
+                        # Calculate the weight as an approximation to the
+                        # natural neighbor measure (how large is the
+                        # difference between the natural boundary and the
+                        # new boundary that would be defined by x)
+                        weights.append( max(0,boundary[0] - boundary[1]/2) )
+                    else:
+                        # Calculate the weight of this training point response
+                        ratio = boundary[1] / boundary[0]
+                        weights.append( self.radius(ratio / self.basis_size) )
+                
+            if sum(weights) > 0:
+                guess = sum(np.array(weights)*self.values)/sum(weights)
+            else:
+                guess = 0
+            predictions.append( guess )
+        # print("weights: ",weights)
+        # print("predictions: ",predictions)
+        return predictions
+
+
+if __name__ == "__main__":
+    from util.plotly import Plot
+    mult = 5
+    fun = lambda x: np.cos(x[0]*mult) + np.sin(x[1]*mult)
+    np.random.seed(0)
+    p = Plot()
+    low = 0
+    upp = 1
+    dim = 2
+    plot_points = 2000
+    N = 36
+    random = True
+    if random:
+        x = np.random.random(size=(N,dim))
+    else:
+        N = int(round(N ** (1/dim)))
+        x = np.array([r.flatten() for r in np.meshgrid(np.linspace(low,upp,N), np.linspace(low,upp,N))]).T
+    y = np.array([fun(v) for v in x])
+    p.add("Training Points", *x.T, y)
+    
+    surf = Linn()
+    surf.fit(x,y)
+    p.add_func("VMesh", surf, *([(low,upp)]*dim), plot_points=plot_points)
+    p.plot(file_name="vmesh.html")
+
--- util/optimize.py
+++ util/optimize.py
@@ -1,18 +1,243 @@
+import time, random
 import numpy as np
+from scipy.optimize import minimize as scipy_minimize
 
-# =================================================
-#           Optimization and Minimization          
-# =================================================
+# ====================================================================
+#                  DEFINITION OF AMPGO MINIMIZER     
+# 
+#   AMPGO is a general framework that uses an arbitrary local convex
+# optimization technique to identify globally optimal solutions in a
+# multimin search space by "tunneling" (modifying the objective
+# function to avoid previously identified optimal solutions).
+#   This implementation utilizes the minimize function from
+# scipy.optimize and the "BFGS" minimization technique that
+# approximates the hessian of the objective function (2nd derivative
+# with respect to all dimensions) and jumps to the absolute minimum.
+# 
+# 
+# AMPGO bibtex entry with added URL component
+# 
+# @article{lasdon2010adaptive,
+#   title={Adaptive memory programming for constrained global optimization},
+#   author={Lasdon, Leon and Duarte, Abraham and Glover, Fred and Laguna, Manuel and Mart{\'\i}, Rafael},
+#   journal={Computers \& Operations Research},
+#   volume={37},
+#   number={8},
+#   pages={1500--1509},
+#   year={2010},
+#   publisher={Elsevier},
+#   url={http://leeds-faculty.colorado.edu/glover/Publications/TS%20-%20AMP%20GO%20Constrained%20Global%20Opt%20w%20Lasdon%20et%20al%20.pdf},
+# }
+# 
+# ====================================================================
+EPS1 = 0.02
+EPS2 = 0.1
+MAX_ITER = 100
+MAX_TABU_ITER = 5
+MAX_TABULIST_SIZE = 5
+LOCAL_TOLERANCE = 1e-8
+# Local minimum solver function, returns new solution and obj. value
+def LOCAL_SOLVER(objfun, initial, bounds, args=()):
+    result = scipy_minimize(objfun, initial, args=args, method="L-BFGS-B",
+                            bounds=bounds, tol=LOCAL_TOLERANCE)
+    return result["x"], result["fun"]
+# Sub-problem used to escape local minima
+def TUNNEL(solution, objective, aspiration, tabulist):
+    numerator = (objective(solution) - aspiration)**2
+    denominator = 1.0
+    for tabu in tabulist:
+        denominator *= np.sqrt(np.sum((solution - tabu)**2))
+    if denominator == 0: return float('inf')
+    return numerator / denominator
+# Drop the point that is farthest away from the new point
+def DROP_TABU_POINTS(solution, tabulist):
+    if len(tabulist) >= MAX_TABULIST_SIZE:
+        # Drop the farthest points
+        distance = np.sqrt(np.sum(
+            (tabulist - solution)**2, axis=1))
+        index = np.argmax(distance)
+        tabulist.pop(index)
+    tabulist.append(solution)
 
+# Pre:  "objective" a numpy array and returns an object that has a
+#         comparison operator 
+#       "halt" is a function that takes no parameters and returns
+#         "False" when the algorithm should continue and "True" when
+#         it should halt
+#       "bounds" is a list of tuples (lower,upper) for each parameter
+#       "solution" is a valid initial solution for this algorithm (in
+#         numpy array format) and can be modified with expected side
+#         effects outside the scope of this algorithm 
+#       "args" is a set of arguments to be passed to the objective
+#         function after the first parameter (a solution)
+# Post: Returns an (objective value, solution) tuple, best achieved
+def AMPGO(objective, halt, bounds, solution,
+          max_tabu_iter=MAX_TABU_ITER, args=tuple()):
+    calc_obj = lambda sol: objective(sol, *args)
+    tabulist = [solution]
+    lower = np.array([b[0] for b in bounds])
+    upper = np.array([b[1] for b in bounds])
+    best_obj = calc_obj(solution)
+    best_sol = solution.copy()
+    new_sol, obj_val = LOCAL_SOLVER(calc_obj, solution, bounds)
+    # Record this solution if it is the best
+    if obj_val < best_obj:
+        best_sol = new_sol.copy()
+        best_obj = obj_val
+    while not halt():
+        # Drop old points and add the new one
+        DROP_TABU_POINTS(new_sol, tabulist)
+        # Use an altered objective function and randomization to escape
+        # current local minimum, attempt this "max_tabu_iter" times
+        for i in range(max_tabu_iter):
+            if halt(): break
+            # Generate a random new solution by picking a random search
+            # direction. Walk "EPS2"*length of current solution.
+            r = np.random.uniform(-1.0, 1.0, size=(solution.shape[0],))
+            beta = EPS2 * np.linalg.norm(new_sol) / np.linalg.norm(r)
+            if np.abs(beta) < LOCAL_TOLERANCE: beta = EPS2
+            new_sol = new_sol + beta*r
+            # Put the new solution back into bounds
+            new_sol = np.where(new_sol < lower, lower, new_sol)
+            new_sol = np.where(new_sol > upper, upper, new_sol)
+            # Generate the aspired objective function value
+            aspiration = best_obj - EPS1*(1.0 + np.abs(best_obj))
+            tunnel_args = (calc_obj, aspiration, tabulist)
+            # Run local solver on sub-problem ("_" is because the obj
+            # value is not the actual obj_value for the function)
+            new_sol, _ = LOCAL_SOLVER(TUNNEL, new_sol, bounds,
+                                      args=tunnel_args)
+            obj_val = calc_obj(new_sol)
+            if obj_val < best_obj:
+                best_sol = new_sol.copy()
+                best_obj = obj_val
+                break
+    return best_obj, best_sol
+# ====================================================================
+# ====================================================================
+
+
+# ====================================================================
+#              DEFINITION OF ADAPTIVE NORMAL MINIMIZER     
+# 
+#   This is an optimization algorithm (coincidentally similar to
+# annealing) that performs competitively with AMPGO on simple test
+# problems. Empirically it has been observed to perform poorly in many
+# dimensions.
+#   This algorithm works by searching along individual dimensions
+# using random normal distributions. Failure causes the standard
+# deviations to shrink (and search locally). Success causes the
+# standard deviations to groaw (and search farther away).
+# 
+# 
+# Adaptive Normal bibtex entry with added URL component
+# 
+# @inproceedings{lux2016convergence,
+#   title={Convergence Rate Evaluation of Derivative-Free Optimization Techniques},
+#   author={Lux, Thomas},
+#   booktitle={International Workshop on Machine Learning, Optimization and Big Data},
+#   pages={246--256},
+#   year={2016},
+#   organization={Springer},
+#   url={https://link.springer.com/chapter/10.1007/978-3-319-51469-7_21},
+# }
+# 
+# ====================================================================
+ACCURACY_POW_2 = 10
+DIMS_TO_MODIFY = 1
+# 
+# Pre:  "objective" a numpy array and returns an object that has a
+#         comparison operator 
+#       "halt" is a function that takes no parameters and returns
+#         "False" when the algorithm should continue and "True" when
+#         it should halt
+#       "bounds" is a list of tuples (lower,upper) for each parameter
+#       "solution" is a valid initial solution for this algorithm (in
+#         numpy array format) and can be modified with expected side
+#         effects outside the scope of this algorithm 
+# Post: Returns an (objective value, solution) tuple, best achieved
+def AdaptiveNormal(objective, halt, bounds, solution,
+                   exhaustion_limit=ACCURACY_POW_2,
+                   num_dim=DIMS_TO_MODIFY, args=tuple()):
+    exhaustion = 0
+    obj_value = objective(solution, *args)
+    # Useful variables for searching
+    lower = np.array([l for l,u in bounds])
+    upper = np.array([u for l,u in bounds])
+    sprawl = upper - lower
+    # Main search loop
+    while not halt():            
+        if exhaustion == 0:
+            # Search globally with random solution
+            new_sol = np.random.random( (len(bounds),) ) * sprawl + lower
+        else:
+            # Search locally with small random modifications
+            for i in range(num_dim):
+                param = np.random.randint( 0, len(bounds) )
+                stdev = sprawl[param] / 2**exhaustion
+                new_sol = solution.copy()
+                new_sol[param] = np.random.normal( solution[param], stdev )
+                # Put the new solution back into bounds
+                new_sol[param] = max(lower[param], new_sol[param])
+                new_sol[param] = min(upper[param], new_sol[param])
+        # Calculate the objective value of the new solution
+        new_obj_value = objective(new_sol, *args)
+        if new_obj_value < obj_value:
+            # If it is better, keep it and reset exhaustion
+            exhaustion = 0
+            solution = new_sol
+            obj_value = new_obj_value
+        else:
+            # Otherwise increment exhaustion
+            exhaustion += 1 
+        # Check to see if exhaustion has gotten too large
+        if exhaustion >= exhaustion_limit:
+            exhaustion = 0
+    return (obj_value, solution)
+# ====================================================================
+# ====================================================================
+
+
+# ====================================================================
+#                     A PURELY RANDOM MINIMIZER
+# 
+#   Generates random solutions inside of bounds, keeping track of best
+# solution obtaiend.
+# 
+# Pre:  "objective" a numpy array and returns an object that has a
+#         comparison operator 
+#       "halt" is a function that takes no parameters and returns
+#         "False" when the algorithm should continue and "True" when
+#         it should halt
+#       "bounds" is a list of tuples (lower,upper) for each parameter
+#       "solution" is a valid initial solution for this algorithm (in
+#         numpy array format) and can be modified with expected side
+#         effects outside the scope of this algorithm 
+# Post: Returns an (objective value, solution) tuple, best achieved
+def Random(objective, halt, bounds, solution, args=tuple()):
+    bound_range = np.array([u-l for u,l in bounds])
+    bound_shift = np.array([l for u,l in bounds])    
+    random_solution = lambda: ( np.random.random( (len(bounds),) )
+                                * bound_range + bound_shift )
+    best_solution = random_solution()
+    best_obj = objective(best_solution, *args)
+    while not halt():
+        new_solution = random_solution()
+        new_obj = objective(new_solution, *args)
+        if new_obj < best_obj:
+            best_solution = new_solution
+            best_obj = new_obj
+    return (best_obj, best_solution)
+# ====================================================================
+# ====================================================================
+
+# ====================================================================
+#            Generic Optimization and Minimization Tracking          
+# 
 DEFAULT_SEARCH_SIZE = 2.0**10
-DEFAULT_MAX_TIME_SEC = 1
+DEFAULT_MAX_TIME_SEC = 0.5
 DEFAULT_MIN_STEPS = 10
-DEFAULT_MIN_IMPROVEMENT = 0.0000
-
-import time
-# from .DiRect import DiRect as optimize
-from .algorithms.AMPGO import AMPGO as optimize
-
+DEFAULT_MIN_IMPROVEMENT = 0.0
 # Class for tracking convergence of optimization algorithm
 class Tracker:
     # Initialization of storage attributes
@@ -32,21 +257,29 @@ class Tracker:
         self.start = None
 
     # Wrapper for the objective function that tracks progress
-    def check(self,sol):
+    def check(self, sol, *args, **kwargs):
         # Initialize starting time if it hasn't been done
         if self.start == None: self.start = time.time()
-        # Don't execute the objective function if time has been exceeded
-        if time.time() - self.start > DEFAULT_MAX_TIME_SEC: return float('inf')
+        # Don't execute the objective function if stopping criteria met
+        if self.done(): return float('inf')
         # Calculate the new objective value
         self.tries += 1
-        new_obj = self.obj(sol)
+        new_obj = self.obj(sol, *args, **kwargs)
         # Record relevant statistics
         if new_obj > self.max_obj: self.max_obj = new_obj
         if new_obj <= self.min_obj:
-            self.record.append(new_obj)
+            self.record.append((new_obj, self.tries, sol))
             self.min_obj = new_obj
             self.best_sol = sol
-            if self.display: print(new_obj, sol)
+            if self.display: 
+                if (self.tries == 1): 
+                    print()
+                    print("Formatted convergence printout:")
+                    print("","[delay] ([attempts]):\t[obj] with [solution]")
+                    print()
+                if (len(self.record) == 1): delay = self.tries
+                else: delay = self.tries - self.record[-2][1]
+                print("","%i (%i): \t%.3e with %s"%(delay, self.tries, new_obj, list(sol)))
         return new_obj
 
     # Function that returns "True" when optimization is over
@@ -57,44 +290,78 @@ class Tracker:
         # Next, check for convergence
         converged = False
         if len(self.record) > self.min_steps:
-            change = [abs(v - self.min_obj) for v in self.record[-self.min_steps:]]
-            divisor = max(self.record) - min(self.record)
+            change = [abs(o - self.min_obj) for (o,t,s) in self.record[-self.min_steps:]]
+            divisor = max(self.record)[0] - min(self.record)[0]
             if divisor == 0: 
+                # This means max_obj == min_obj, clearly not converged
                 converged = False
             else:
+                # Check if the relative convergence of the most recent
+                # window of solutions is slower than a prescribed value
                 converged = max(change) / divisor < self.min_change
         return converged
+# ====================================================================
+# ====================================================================
 
-# Minimization function
-def minimize(objective, solution, bounds=None,
+
+# Minimize arbitrary objective function, optionally given an initial
+# solution, bounds, max computation time, minimum number of steps, or
+# a minimum improvement stopping criteria.
+# 
+# INPUT:
+#   objective -- A callable function that takes a numpy array of
+#                the same length as "solution" and returns a float.
+#   solution  -- A 1D numpy array (or list) that is an initial
+#                starting point for the search (also implicilty
+#                defines the number of dimenions for the problem)
+# 
+# OPTIONAL INPUT:
+#   bounds          -- [(low_1, upp_1), ..., (low_d, upp_d)]
+#                      A list of tuples of lower and upper bounds for
+#                      each dimension of valid candidate solutions
+#   args            -- A tuple of arguments to pass to the objectivev function
+#   max_time        -- The maximum amount of computation time for optimization
+#   min_steps       -- The minimum number of evaluations of the objective function
+#   min_improvement -- The minimum improvement needed to continue searching
+#   display         -- Boolean, if True then try to use plotly to render
+#                      a plot of the best solution convergence by
+#                      iterations, otherwise print a console summary.
+#   method          -- A minimization function that takes as
+#                      parameters, (objective function, halting
+#                      function , bounds, initial solution, obj args)
+#                      and will find a candidate minimum solution.
+#      This file provides the methods:
+#       AMPGO (default) - BFGS with tunneling away from prior solutions,
+#                         best for smooth locally convex multi-min spaces
+#       AdaptiveNormal  - Quasi simulated annealing with exhaustion,
+#                         best for noisy multi-min spaces
+#       Random          - Pure random baseline for comparison, best for
+#                         discontinuous and noisy spaces.
+# 
+# OUTPUT:
+#   solution        -- The best solution identified by the minimizer provided
+#                      the initial restraints + stopping condition(s).
+def minimize(objective, solution, bounds=None, args=tuple(),
              max_time=DEFAULT_MAX_TIME_SEC,
              min_steps=DEFAULT_MIN_STEPS,
-             min_improvement=DEFAULT_MIN_IMPROVEMENT, plot=False):
-    solution = np.array(list(map(float,solution)))
+             min_improvement=DEFAULT_MIN_IMPROVEMENT, display=False,
+             method=AMPGO):
+    # Convert the solution into a float array (if it's not already)
+    solution = np.asarray(solution, dtype=float)
 
     # Generate some arbitrary bounds
     if type(bounds) == type(None):
-        upper = solution + np.array([DEFAULT_SEARCH_SIZE]*len(solution))
-        lower = solution - np.array([DEFAULT_SEARCH_SIZE]*len(solution))
+        upper = solution + np.ones((len(solution),))*DEFAULT_SEARCH_SIZE
+        lower = solution - np.ones((len(solution),))*DEFAULT_SEARCH_SIZE
         bounds = list(zip(lower, upper))
 
     # Initialize a tracker for halting the optimization
-    t = Tracker(objective, max_time, min_steps, min_improvement, plot)
+    t = Tracker(objective, max_time, min_steps, min_improvement, display)
 
     # Call the optimization function and get the best solution
-    optimize(t.check, t.done, bounds, solution)
 
-    if plot:
-        import pygal
-        CUSTOM_CSS_FILE = 'file://'+os.path.expanduser("~")+os.path.join(
-            "Git", "pyvis", "pyvis", "Visualize",
-            "Value_Distribution", "no_dot.css")
-        config = pygal.Config()
-        config.css.append(CUSTOM_CSS_FILE)
-        plot = pygal.XY(config)
-        plot.add("Objective", list(zip(range(len(t.record)),t.record)))
-        plot.render_in_browser()
-        
+    method(t.check, t.done, bounds, solution, args=args)
 
+    if display: print()
     return t.best_sol
 
