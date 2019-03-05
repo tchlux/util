6bac2472b699ef11f4074aefef8b92f836922766
Thomas Lux <thomas.ch.lux@gmail.com>
2019-03-05 13:01:16 -0500

    Fixed some compatibility issues and added a test case for plotting.

--- util/approximate/support_vector_machine/__init__.py
+++ util/approximate/support_vector_machine/__init__.py
@@ -8,11 +8,25 @@ SVR_KERNEL = "poly"
 # ==================================
 class SVR(Approximator):
     def __init__(self, *args, kernel=SVR_KERNEL, **kwargs):
+        # Disable the future warnings that SVR gives.
+        import warnings
+        warnings.filterwarnings("ignore", category=FutureWarning)
+        # Import the model from sklearn.
         from sklearn.svm import SVR
         self.SVR = SVR
         kwargs.update(dict(kernel=kernel))
         self.svr = self.SVR(*args, **kwargs)
-    def fit(self, *args, **kwargs):
-        return self.svr.fit(*args, **kwargs)
-    def predict(self, *args, **kwargs):
-        return self.svr.predict(*args, **kwargs)
+    def _fit(self, x, y, *args, **kwargs):
+        if (y.shape[1] > 1): raise(Exception("SVR only supports 1D output."))
+        y = y[:,0]
+        return self.svr.fit(x, y, *args, **kwargs)
+    def _predict(self, *args, **kwargs):
+        output = self.svr.predict(*args, **kwargs)
+        if (len(output.shape) == 1): output = output[:,None]
+        return output
+
+if __name__ == "__main__":
+    from util.approximate.testing import test_plot
+    m = SVR()
+    p, x, y = test_plot(m, random=True, N=200)
+    p.show()
