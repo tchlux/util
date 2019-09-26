64c01f414191313b5ca52e43257cd6238dada47a
Thomas Lux <thomas.ch.lux@gmail.com>
2019-09-26 12:47:51 -0500

    Removed ball tree and turned it into its own package.

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
-# Custom error (in case user does not have setuptools)
-class DependencyError(Exception): pass
-
 # Try to import setuptools (if it fails, the user needs that package)
 try: 
     from setuptools import setup, find_packages
 except:
+    # Custom error (in case user does not have setuptools)
+    class DependencyError(Exception): pass
     raise(DependencyError("Missing python package 'setuptools'.\n  pip install --user setuptools"))
 
 import os
+
 # Convenience function for reading information files
-def read(f_name, dir_name="about"):
+def read(f_name, empty_lines=False):
     text = []
-    with open(os.path.join(dir_name, f_name)) as f:
+    with open(os.path.join(package_about, f_name)) as f:
         for line in f:
-            line = line.strip()
-            if (len(line) > 0) and (line[0] != "#"):
-                text.append(line)
+            line = line.strip("\n")
+            if (not empty_lines) and (len(line.strip()) == 0): continue
+            if (len(line) > 0) and (line[0] == "%"): continue
+            text.append(line)
     return text
+
+# Go to the "about" directory in the package directory.
+package_about = ""
+package_name = read("package_name.txt")[0]
+package_about = os.path.join(os.path.dirname(os.path.abspath(__file__)),package_name,"about")
 
 if __name__ == "__main__":
     #      Read in the package description files     
     # ===============================================
-    package = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+    package = package_name
     version =read("version.txt")[0]
     description = read("description.txt")[0]
     requirements = read("requirements.txt")
@@ -30,35 +36,35 @@ if __name__ == "__main__":
     name, email, git_username = read("author.txt")
 
     # Translate the git requirements to proper requirements.
-    dependency_links = [r for r in requirements if "github.com" in r]
+    dependency_links = [r for r in requirements if "git+git:" in r[:8]]
     for r in dependency_links:
         try:    pkg_name = r.split("egg=")[1]
         except: raise(DependencyError("GitHub repositories must specify '#egg=<package-name>' at the end."))
-        requirements[requirements.index(r)] = " ".join([pkg_name,"@","git+https://" + r.split("://")[1]])
-
+        requirements[requirements.index(r)] = pkg_name + " @ git+https://" + r.split("://")[1]
+    # Call "setup" to formally set up this module.
     setup(
         author = name,
         author_email = email,
         name=package,
         packages=find_packages(exclude=[]),
+        include_package_data=True,
         install_requires=requirements,
         version=version,
-        description = description,
-        keywords = keywords,
-        classifiers=classifiers,
-        include_package_data=True,
         url = 'https://github.com/{git_username}/{package}'.format(
             git_username=git_username, package=package),
         download_url = 'https://github.com/{git_username}/{package}/archive/{version}.tar.gz'.format(
             git_username=git_username, package=package, version=version),
-        python_requires = '>=3.0',
+        description = description,
+        keywords = keywords,
+        python_requires = '>=3.6',
         license='MIT',
+        classifiers=classifiers
     )
 
 
-# When using Homebrew to install this library, the following
-# additional packages may be required to make cv2 import correctly:
 # 
-#   brew install gtk+
-#   brew install linuxbrew/xorg/libsm
+# Install specific versions of a package with something like:
+# 
+#   pip install https://github.com/tchlux/packager/archive/1.0.0.zip
+# 
 # 
--- util/approximate/nearest_neighbor/bt/__init__.py
+++
@@ -1,2 +0,0 @@
-
-from .balltree import btmod, BallTree
